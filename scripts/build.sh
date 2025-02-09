2cf3cce46965f8eca5223a03720640cca5189985
SchoolyB <marshallbcodes@gmail.com>
2025-02-09 11:33:31 -0600

    Revert changes to build shell script

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,15 +9,7 @@ cd "$DIR/.."
 # Change to the src directory
 cd src
 
-# Determine the executable extension based on the operating system
-if [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "win"* ]]; then
-    # Windows (Git Bash or Windows Subsystem)
-    EXECUTABLE_EXT=".exe"
-else
-    # Mac or Linux
-    EXECUTABLE_EXT=".bin"
-fi
-
+echo -e "$(tput setaf 3)Building OstrichDB...$(tput sgr0)"
 # Build the project in the src directory
 odin build main
 
@@ -26,21 +18,21 @@ if [ $? -eq 0 ]; then
     echo "$(tput setaf 2)Build successful$(tput sgr0)"
     
     # Try to create bin directory and move the executable
-    if mkdir -p ../bin 2>/dev/null && mv "main$EXECUTABLE_EXT" ../bin/ 2>/dev/null; then
+    if mkdir -p ../bin 2>/dev/null && mv "main.bin" ../bin/ 2>/dev/null; then
         cd ../bin
     else
         echo "$(tput setaf 1)Could not move executable to bin directory. Running from src.$(tput sgr0)"
     fi
     
     # Run the program
-    ./main$EXECUTABLE_EXT
+    ./main.bin
 
     # Capture the exit code
     exit_code=$?
 
     # Check the exit code
     if [ $exit_code -ne 0 ]; then
-        echo "$(tput setaf 1)Program exited with code $exit_code$(tput sgr0)"
+        echo "$(tput setaf 1)OstrichDB exited with code $exit_code$(tput sgr0)"
     fi
 
     # Return to the project root directory
