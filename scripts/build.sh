9e63e80e0c5ebb9e5453df59a6fbcea96fc548cd
SchoolyB <marshallbcodes@gmail.com>
2025-02-25 18:00:20 -0600

    Update build scripts/move main package/add install script

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,18 +13,18 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Change to the project root directory
 cd "$DIR/.."
 
-# Change to the src directory
-cd src
 
+# Print the current directory
+# echo "Current directory: $(pwd)"
 # Build the project in the src directory
-# odin build main
+odin build main
 
 # Check if build was successful
 if [ $? -eq 0 ]; then
     echo "$(tput setaf 2)Build successful$(tput sgr0)"
 
     # Try to create bin directory and move the executable
-    if mkdir -p ../bin 2>/dev/null && mv main.bin ../bin/ 2>/dev/null; then
+    if mkdir -p ./bin 2>/dev/null && mv main.bin ./bin/ 2>/dev/null; then
         echo "$(tput setaf 2)Successfully moved executable to bin directory$(tput sgr0)"
     else
         echo "$(tput setaf 1)Could not move executable to bin directory$(tput sgr0)"
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-#This script builds and runs OstrichDB. Mostly used for development purposes.
-
+# Used to run OstrichDB without rebuilding. Mostly used for development purposes.
 #Author: Marshall A Burns
 #GitHub: @SchoolyB
 #License: Apache License 2.0 (see LICENSE file for details)
 #Copyright (c) 2024-Present Marshall A Burns and Solitude Software Solutions LLC
-
 
 # Get the directory of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -14,23 +12,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Change to the project root directory
 cd "$DIR/.."
 
-# # Change to the src directory
-# cd src
+# Change to the bin directory
+cd bin
 
-# Build the project in the src directory
-odin build main
-
-# Check if build was successful
-if [ $? -eq 0 ]; then
-    echo "$(tput setaf 2)Build successful$(tput sgr0)"
-
-    # Try to create bin directory and move the executable
-    if mkdir -p ./bin 2>/dev/null && mv main.bin ./bin/ 2>/dev/null; then
-        cd bin
-    else
-        echo "$(tput setaf 1)Could not move executable to bin directory. Running from src.$(tput sgr0)"
-    fi
-
+# Check if the executable exists
+if [ -f "main.bin" ]; then
     # Run the program
     ./main.bin
 
@@ -44,7 +30,6 @@ if [ $? -eq 0 ]; then
 
     # Return to the project root directory
     cd "$DIR/.."
-
 else
-    echo "$(tput setaf 1)Build failed$(tput sgr0)"
+    echo "$(tput setaf 1)Executable not found. Please build the project first.$(tput sgr0)"
 fi
