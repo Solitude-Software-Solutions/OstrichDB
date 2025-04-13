196ad8f0ae8d3df6f1bd454145604181a582614c
SchoolyB <marshallbcodes@gmail.com>
2025-04-13 14:57:30 -0500

    Update local_build_run script(#249)

--- scripts/local_build_run.sh
+++ scripts/local_build_run.sh
@@ -14,34 +14,40 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR/.."
 
 
+# Go into nlp package and build NLP Go library
+cd "src/core/nlp"
+go mod init main
+go mod tidy
+go build -buildmode c-shared -o nlp.dylib
+
+# Go back to root dir
+cd "$DIR/.."
 # Build the project, if DEV_MODE is false then shit breaks so LEAVE IT THE FUCK ALONE - Marshall
 odin build main -define:DEV_MODE=true
 
 # Check if build was successful
 if [ $? -eq 0 ]; then
     echo "$(tput setaf 2)Build successful$(tput sgr0)"
-
-    # Try to create bin directory and move the executable
-    if mkdir -p ./bin 2>/dev/null && mv main.bin ./bin/ 2>/dev/null; then
+    # Create bin directory if it doesn't exist
+    mkdir -p ./bin
+    # Move the NLP library from src/core/nlp to the bin dir
+    cp src/core/nlp/nlp.dylib ./bin/
+    # Try to move the executable
+    if mv main.bin ./bin/ 2>/dev/null; then
         cd bin
     else
         echo "$(tput setaf 1)Could not move executable to bin directory. Running from src.$(tput sgr0)"
     fi
-
     # Run the program
     ./main.bin
-
     # Capture the exit code
-    exit_code=$?
-
+    exitCode=$?
     # Check the exit code
-    if [ $exit_code -ne 0 ]; then
-        echo "$(tput setaf 1)Program exited with code $exit_code$(tput sgr0)"
+    if [ $exitCode -ne 0 ]; then
+        echo "$(tput setaf 1)Program exited with code $exitCode$(tput sgr0)"
     fi
-
     # Return to the project root directory
     cd "$DIR/.."
-
 else
     echo "$(tput setaf 1)Build failed$(tput sgr0)"
 fi
