60fb17a498d8ca2c59f909edd5e1df5d4da3c86f
SchoolyB <marshallbcodes@gmail.com>
2025-05-09 10:47:03 -0500

    Add contributors to scripts in notices

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,10 @@
 
 #Author: Marshall A Burns
 #GitHub: @SchoolyB
+
+#Contributors
+#    @CobbCoding1
+
 #License: Apache License 2.0 (see LICENSE file for details)
 #Copyright (c) 2024-Present Marshall A Burns and Solitude Software Solutions LLC
 
